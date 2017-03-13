10357d00f635a558a2ce0cbaedd0c324853a944c
Dell <Dell@DELL-PC>
2017-03-13 11:25:36 +0100

    Implementation de  la classe CabinetKit et Order

--- Kitbox Class Libary/Kitbox Class Libary/CabinetKit.cs
+++ Kitbox Class Libary/Kitbox Class Libary/CabinetKit.cs
@@ -15,7 +15,7 @@ namespace Kitbox_Class_Libary
         private double identical_rack_height;
         private List<Racks> list_of_racks;
 
-        //Properties
+        //Properties/Accessors
         public List<Racks> List_of_racks
         {
             get { return list_of_racks; }
@@ -49,27 +49,69 @@ namespace Kitbox_Class_Libary
         }
 
         //Methods
+
+        //
         public void AddRack(Rack rack)
         {
 
         }
+
+        //
         public void SelectDepth(double depth)
         {
             Depth = depth;
         }
+
+        //
         public void SelectWidth(double width)
         {
             Width = width;
         }
+
+        //
         public void Select_Identical_RackHeight(double height)
         {
             Identical_rack_height = height;
         }
+
+        //
         public void Select_SteelBars_colour(string colour )
         {
 
         }
+
+        //
         public void Select_DoorAdd(bool doorChoice)
+        {
+
+        }
+
+        //
+        public void Select_DoorHandleAdd(bool doorHandleChoice)
+        {
+
+        }
+
+        //
+        public void Select_DoorColour(bool doorHandleChoice)
+        {
+
+        }
+
+        //
+        public void Select_DoorMaterial(string doorMaterial )
+        {
+
+        }
+
+        //
+        public void Select_PanelsColours(string rearPanel, string topPanel, string bottomPanel, string leftPanel, string RightPanel)
+        {
+
+        }
+
+        //
+        public void Select_CrossbeamsColours(string rearCrossbeam, string frontalCrossbeam, string LeftCrossbeam, string rightCrossbeam)
         {
 
         }
