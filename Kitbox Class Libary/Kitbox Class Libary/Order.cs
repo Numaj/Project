35ec7267512b63e04d0d66325b2b5512e39f6c31
Dell <Dell@DELL-PC>
2017-03-15 23:06:39 +0100

    Added comments to order class.

--- Kitbox Class Libary/Kitbox Class Libary/Order.cs
+++ Kitbox Class Libary/Kitbox Class Libary/Order.cs
@@ -8,9 +8,18 @@ namespace Kitbox_Class_Libary
 {
     class Order
     {
+        /*------------------------------------------------------------------------------------------------
+         * This class represents an order created by a Kitbox client.
+         * An order contains a list of cabinet kits which contains the full list of elements
+         * needed to construct the cabinet kit.
+         * Last update 15/03/2017 - Added comments by Jean-Rene N.
+         * -----------------------------------------------------------------------------------------------
+         */
+
         //Fields
         private double totalprice;
         private string orderID;
+        private string state;                            //Order can be Active or Passive
         private List<CabinetKit> list_of_cabinetkits;
 
         //Properties/Accessors
