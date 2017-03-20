040047f26ecd930ef42a0de7dfff092f5c7cda9f
Catry <catry>
2017-03-20 10:37:58 +0100

    ajout classe client

---
+++ Kitbox Class Libary/Kitbox Class Libary/Client.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kitbox_classes
+{
+    class Client : User
+    {
+        List<int> list; // int aussi à remplacer par Order
+
+        public int AddOrder() // remplacer int par Order
+        {
+            int jeanmarie = 1; // idem
+            return jeanmarie; 
+        }
+    }
+
+}
